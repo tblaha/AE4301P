dceefae0f1306e45808746d9694a5597b3b0aec3
tblaha <tblaha@TillArch.TillNet>
2020-11-10 13:02:37 +0100

    Clean Master for now! Automatic setup that fixes path and mexes the c-file, if necessary.

---
+++ setup.m
@@ -0,0 +1,30 @@
+%% make path
+
+addpath("F16Sim")
+
+
+%% Check for mex and if not existent, mex it outselves
+
+if exist('nlplant') ~= 3
+    disp("Mex doesn't exist in F16Sim directory")
+    disp("")
+    disp("Attempting Compilation")
+    disp("----------------")
+    
+    pause on
+    pause(2)
+    pause off
+    
+    cd F16Sim
+    
+    try
+        o = evalc("mex nlplant.c");
+    catch
+        if exist('nlplant') == 3
+            disp("Mex Produced and error; but we can continue")
+        end
+    end
+    
+    cd ../
+       
+end
