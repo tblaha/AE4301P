d5f4074e440ec00c3d18a46cc73824e641b833c3
Till Blaha <till.blaha@web.de>
2020-11-22 16:59:42 +0100

    Table output of Characteristics; Prettier plots; Integration with trim_lin and gen_fc

--- OL.m
+++ OL.m
@@ -1,59 +1,130 @@
-load('SS_Std_LoFi_4DoF.mat');
+warning('off','all')
+close("all")
+clear
+
+%% setup
+
+% check if run from correct path:
+[~, lastdir, ~] = fileparts(pwd);
+if ~strcmp(lastdir, "AE4301P")
+    error(strcat("Please run ",...
+                 mfilename,...
+                 " from the root directory of this project"...
+                 ))
+end
+
+% load Steady State system; if it doesn't exist, run trim_lin.m
+fprintf("OL: Checking if SS matrices exist...")
+if exist("fc_files/SS_Std_LoFi_4DoF.mat", 'file') > 0
+    fprintf("OK\n")
+    
+    load('fc_files/SS_Std_LoFi_4DoF.mat');
+else
+    fprintf("failed: try to run trim-lin to generate them...\n")
+    
+    run trim_lin.m
+    
+    fprintf("OL: Importing SS matrix...")
+    
+    load('fc_files/SS_Std_LoFi_4DoF.mat');
+    fprintf("OK, recovered\n")
+end
+
+% make folder for the plots; if it doesn't exist yet
+mkdir OL_plot_files
+
+% set visibility of on-screen plots (they are saved as eps anyway)
+show_plots = 'on';
+
+% set figure pixes sizes
+fig_size = [600 400]; % width, height
+fig_pos = [100 100 100+fig_size(1) 100+fig_size(2)];
 
 
-%figure(1)
-map_long = pzmap(SS_long);
+%% Start
 
-%figure(2)
-map_lat = pzmap(SS_lat);
 
-[nat_freq_long,damping_long,poles_long] = damp(SS_long);
-[nat_freq_lat,damping_lat,poles_lat] = damp(SS_lat);
-T_half_long = log(0.5)./real(poles_long);
-T_half_lat = log(0.5)./real(poles_lat);
-time_cons_long = -1./real(poles_long);
-time_cons_lat = -1./real(poles_lat);
 
-%Longitudinal
-%phugoid, pitch rate, step input
-SS_pitchrate = SS_long(4,2);
-plot_long_pitchrate = figure(1);
-step(SS_pitchrate);
-mkdir 'OL_plot_files'  
-filename = strcat("OL_plot_files/", 'Pitchrate_phugoid_long');
-saveas(plot_long_pitchrate, filename, 'epsc');
 
-%short period, pitch rate, step input
-SS_pitchrate = SS_long(4,2);
-plot_long_pitchrate = figure(2);
-step(SS_pitchrate,10);
-mkdir 'OL_plot_files'  
-filename = strcat("OL_plot_files/", 'Pitchrate_sp_long');
-saveas(plot_long_pitchrate, filename, 'epsc');
 
-%Lateral
-%dutch roll, rudder input, yaw rate
-SS_yawrate = SS_lat(4,3);
-plot_lat_yawrate = figure(3);
-impulse(SS_yawrate,15);
-mkdir 'OL_plot_files'  
-filename = strcat("OL_plot_files/", 'Yawrate_dr_lat');
-saveas(plot_lat_yawrate, filename, 'epsc');
+%% Numeric Characteristics
 
-%spiral, aileron input, roll angle
-SS_rollangle = SS_lat(2,2);
-plot_lat_rollrate = figure(4)
-impulse(SS_rollangle)
-mkdir 'OL_plot_files'  
-filename = strcat("OL_plot_files/", 'rollrate_spiral_lat');
-saveas(plot_lat_rollangle, filename, 'epsc');
+fprintf("OL: Calculate table of numerical characteristics...")
 
-%aperiodic roll, aileron input, roll rate
-SS_rollrate = SS_lat(3,2);
-plot_lat_rollrate = figure(5)
-step(SS_rollrate,10)
-mkdir 'OL_plot_files'  
-filename = strcat("OL_plot_files/", 'rollrate_aper_lat');
-saveas(plot_lat_rollrate, filename, 'epsc');
+%%%%%% longidutinal
 
+[omega_long, zeta_long, p_long] = damp(SS_long);
+tau_long = -1./real(p_long);
+T2_long  = log(2)*tau_long;
+
+
+
+%%%%%% lateral
+
+[omega_lat, zeta_lat, p_lat] = damp(SS_lat);
+tau_lat = -1./real(p_lat);
+T2_lat  = log(2)*tau_lat;
+
+
+
+%%%%% build output table
+% with a table we have all info in a single object (char_tab) and we 
+% can access elements like this:
+%
+%%% show entire table:
+% char_tab
+%
+%%% example to get a scalar
+% char_tab.NaturalFrequency("Phugoid");
+
+%%% example to get a column
+% char_tab.DampingRatio;
+
+% names of the modes (in order)
+long_modes = ["Phugoid", "Phugoid Conjugate", ...
+              "Short Period", "Short Period Conjugate"
+              ];
+lat_modes = ["Spiral", "Aperiodic Roll", "Dutch Roll", "Dutch Roll Conjugate"];
+
+% variable names, units, symbols in order
+var_names = ["NaturalFrequency", "DampingRatio", ...
+                    "TimeConstant", "HalfTime", "Pole"];
+var_units = {'rad s^-1', '', 's', 's', 's^-1'};
+var_symbols = ["\omega_n", "\zeta", "\tau", "T_2", "p"];
+
+% make table
+char_tab = table([omega_long; omega_lat],...
+                 [zeta_long; zeta_lat],...
+                 [tau_long; tau_lat],...
+                 [T2_long; T2_lat],...
+                 [p_long; p_lat],...
+                 'VariableNames', var_names,...
+                 'RowNames', [long_modes, lat_modes]);
+             
+% set additional properties
+char_tab.Properties.VariableUnits = var_units;
+char_tab = addprop(char_tab, 'LaTeXSymbols', 'variable');
+char_tab.Properties.CustomProperties.LaTeXSymbols = var_symbols;
+
+
+fprintf("OK\n")
+
+%% PZ plots
+
+fprintf("OL: making Pole Zero maps...")
+    run make_PZ_plots.m
+
+fprintf("OK\n")
+
+
+%% time domain plots
+
+fprintf("OL: making time domain response plots...")
+    run make_time_plots.m
+
+fprintf("OK\n")
+
+%%
+
+fprintf("OL: done\n")
 
---
+++ make_time_plots.m
@@ -0,0 +1,147 @@
+
+%% Longitudinal
+
+%%%%%%% phugoid, pitch rate, step input
+plot_long_pitchrate = figure("Name", "Phugoid",...
+                             "Position", fig_pos,...
+                             "Visible", show_plots);
+
+SS_el_pitchrate = SS_long(4,2);
+
+% set plot options
+p = make_plot_options("Time Response to $1^{\circ}$ Elevator Step",...
+                      "Time t [s]", "Pitch Rate q [$\circ$ / s]");
+
+% plot the step
+stepplot(SS_el_pitchrate, 1000, p);
+base_ax = gca;
+
+% increase line thickness
+base_ax.Children(1).Children(2).LineWidth=1.5;
+
+
+%%%%%%%%% In same plot: Short Period, pitch rate, step input
+
+% create new axes to zoom around Origin
+zoom_ax = axes('position',[.35 .2 .35 .38]);
+
+% set plot options
+p = make_plot_options("Zoom closer to $t=0$",...
+                      "Time t [s]", "Pitch Rate q [$\circ$ / s]");
+p.Title.FontSize = 12; p.XLabel.FontSize = 8; p.YLabel.FontSize = 8;
+
+% plot the step
+stepplot(SS_el_pitchrate, 12, p);
+zoom_ax.Children(1).Children(2).LineWidth=1.5;
+
+% save plot
+filename = strcat("OL_plot_files/", 'Pitchrate_Phugoid_and_sp_long');
+print(plot_long_pitchrate, '-depsc2', '-painters', filename)  % looks better for some reason...
+% saveas(plot_long_pitchrate, filename, 'epsc2');
+
+
+
+% %short period, pitch rate, step input
+% plot_long_pitchrate = figure("Name", "Short Period",...
+%                              "Position", fig_pos,...
+%                              "Visible", show_plots);
+%                          
+% SS_el_pitchrate = SS_long(4,2);
+% step(SS_el_pitchrate, 10);
+% filename = strcat("OL_plot_files/", 'Pitchrate_sp_long');
+% saveas(plot_long_pitchrate, filename, 'epsc');
+
+%% Lateral
+
+%%%%%%% dutch roll, rudder input, yaw rate
+plot_lat_yawrate = figure("Name", "Dutch Roll",...
+                             "Position", fig_pos,...
+                             "Visible", show_plots);
+
+SS_yawrate = SS_lat(4,3);
+
+% set plot options
+p = make_plot_options("Time Response to $1^{\circ} s$ Rudder Impulse",...
+                      "Time t [s]", "Yaw Rate r [$\circ$ / s]");
+
+% plot the impulse
+impulseplot(SS_yawrate,15, p);
+base_ax = gca;
+
+% increase line thickness
+base_ax.Children(1).Children(2).LineWidth=1.5;
+
+% save
+filename = strcat("OL_plot_files/", 'Yawrate_dr_lat');
+print(plot_lat_yawrate, '-depsc2', '-painters', filename)  % looks better for some reason...
+% saveas(plot_lat_yawrate, filename, 'epsc');
+
+
+
+
+%%%%%%% spiral, aileron input, roll angle
+plot_lat_rollangle = figure("Name", "Spiral",...
+                             "Position", fig_pos,...
+                             "Visible", show_plots);
+SS_rollangle = SS_lat(2,2);
+
+% set plot options
+p = make_plot_options("Time Response to $1^{\circ} s$ Aileron Impulse",...
+                      "Time t [s]", "Roll Angle $\phi$ [$\circ$]");
+
+% plot the impulse
+impulseplot(SS_rollangle, 800, p)
+base_ax = gca;
+
+% increase line thickness
+base_ax.Children(1).Children(2).LineWidth=1.5;
+
+% save
+filename = strcat("OL_plot_files/", 'rollrate_spiral_lat');
+print(plot_lat_rollangle, '-depsc2', '-painters', filename)  % looks better for some reason...
+% saveas(plot_lat_rollangle, filename, 'epsc');
+
+
+
+
+%%%%%%% aperiodic roll, aileron input, roll rate
+plot_lat_rollrate = figure("Name", "ApRoll",...
+                             "Position", fig_pos,...
+                             "Visible", show_plots);
+SS_rollrate = SS_lat(3,2);
+
+% set plot options
+p = make_plot_options("Time Response to $1^{\circ}$ Aileron Step",...
+                      "Time t [s]", "Roll Rate p [$\circ$ / s]");
+
+% plot the step
+stepplot(SS_rollrate,10, p)
+base_ax = gca;
+
+% increase line thickness
+base_ax.Children(1).Children(2).LineWidth=1.5;
+
+% save
+filename = strcat("OL_plot_files/", 'rollrate_aper_lat');
+print(plot_lat_rollrate, '-depsc2', '-painters', filename)  % looks better for some reason...
+% saveas(plot_lat_rollrate, filename, 'epsc');
+
+
+
+%% helper functions
+
+
+function p = make_plot_options(Title, XLabel, YLabel)
+    
+    p = timeoptions;
+    
+    p.Title.String = Title;
+    p.XLabel.String = XLabel;
+    p.YLabel.String = YLabel;
+    p.Title.Interpreter = 'latex'; 
+    p.XLabel.Interpreter = 'latex'; 
+    p.YLabel.Interpreter = 'latex';
+    p.Title.FontSize = 18; p.XLabel.FontSize = 16; p.YLabel.FontSize = 16;
+    p.Grid = 'on';
+    
+end
